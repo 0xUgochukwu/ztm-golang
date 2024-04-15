4dcd49e9e7d8170ebfbce659ded8f8a8b0c0adef
0xUgochukwu <0xUgochukwu@gmail.com>
2024-04-15 09:05:59 +0100

    testing

--- src/lectures/exercise/testing/rcv-func_test.go
+++ src/lectures/exercise/testing/rcv-func_test.go
@@ -1,12 +1,69 @@
-//--Summary:
-//  Copy your rcv-func solution to this directory and write unit tests.
+// --Summary:
 //
-//--Requirements:
-//* Write unit tests that ensure:
-//  - Health & energy can not go above their maximums
-//  - Health & energy can not go below 0
-//* If any of your  tests fail, make the necessary corrections
-//  in the copy of your rcv-func solution file.
+//	Copy your rcv-func solution to this directory and write unit tests.
 //
-//--Notes:
-//* Use `go test -v ./exercise/testing` to run these specific tests
+// --Requirements:
+// * Write unit tests that ensure:
+//   - Health & energy can not go above their maximums
+//   - Health & energy can not go below 0
+//   - If any of your  tests fail, make the necessary corrections
+//     in the copy of your rcv-func solution file.
+//
+// --Notes:
+// * Use `go test -v ./exercise/testing` to run these specific tests
+package main
+
+import "fmt"
+import "testing"
+
+type Player struct {
+	health, maxHealth uint
+	energy, maxEnergy uint
+	name              string
+}
+
+func (player *Player) modifyHealth(health uint) {
+	player.health += health
+	if player.health > player.maxHealth {
+		player.health = player.maxHealth
+	}
+
+	fmt.Println("Health changed to", player.health)
+}
+
+func (player *Player) modifyEnergy(energy uint) {
+	player.energy += energy
+	if player.energy > player.maxEnergy {
+		player.energy = player.maxEnergy
+	}
+	fmt.Println("Energy changed to", player.energy)
+}
+
+func TestModifyHealth(t *testing.T) {
+	player := Player{
+		health:    100,
+		maxHealth: 100,
+		energy:    100,
+		maxEnergy: 100,
+		name:      "Kobe",
+	}
+	player.modifyHealth(10)
+
+	if player.health > player.maxHealth {
+		t.Errorf("Health should not exceed maxHealth")
+	}
+}
+
+func TestModifyEnergy(t *testing.T) {
+	player := Player{
+		health:    100,
+		maxHealth: 100,
+		energy:    100,
+		maxEnergy: 100,
+		name:      "Kobe",
+	}
+	player.modifyEnergy(10)
+	if player.energy > player.maxEnergy {
+		t.Errorf("Energy should not exceed maxEnergy")
+	}
+}
