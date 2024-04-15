8f868f6f1acd6e46628ded42114651d3f09c0722
0xUgochukwu <0xUgochukwu@gmail.com>
2024-04-15 09:43:14 +0100

    interfaces

--- src/lectures/exercise/interfaces/interfaces.go
+++ src/lectures/exercise/interfaces/interfaces.go
@@ -21,6 +21,39 @@ package main
 
 import "fmt"
 
-func main() {
+type Directer interface {
+	directVehicle()
+}
 
+type Motorcycle string
+
+type Car string
+
+type Truck string
+
+func (m Motorcycle) directVehicle() {
+	fmt.Println("Motorcycle is directed to the small lift")
+}
+
+func (c Car) directVehicle() {
+	fmt.Println("Car is directed to the standard lift")
+}
+
+func (t Truck) directVehicle() {
+	fmt.Println("Truck is directed to the large lift")
+}
+
+func directVehicles(vehicles []Directer) {
+	for _, vehicle := range vehicles {
+		vehicle.directVehicle()
+	}
+}
+
+func main() {
+	vehicles := []Directer{
+		Motorcycle("Harley Davidson"),
+		Car("Toyota Camry"),
+		Truck("Ford F-150"),
+	}
+	directVehicles(vehicles)
 }
